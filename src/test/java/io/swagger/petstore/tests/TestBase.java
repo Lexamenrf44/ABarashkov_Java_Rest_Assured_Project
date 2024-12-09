e608106b0542c99d6a73ab14267ac05fdd6462e9
lexamenrf44 <alexander.stcstudent@gmail.com>
2024-12-09 23:33:46 +0100

    Added additional instances of controllers to TestBase;

--- src/test/java/io/swagger/petstore/tests/TestBase.java
+++ src/test/java/io/swagger/petstore/tests/TestBase.java
@@ -2,11 +2,15 @@ package io.swagger.petstore.tests;
 
 import com.github.javafaker.Faker;
 import io.swagger.petstore.service.PetController;
+import io.swagger.petstore.service.StoreController;
+import io.swagger.petstore.service.UserController;
 import io.swagger.petstore.spec.Specification;
 
 public class TestBase {
 
     protected final PetController petController = new PetController();
+    protected final UserController userController = new UserController();
+    protected final StoreController storeController = new StoreController();
     protected final Faker faker = new Faker();
 
     static {
